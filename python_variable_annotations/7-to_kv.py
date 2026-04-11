f1b28c15f139b9de8f73386ae4e96d8c660ff976
NicolasDS83600 <nico836001@outlook.fr>
2026-04-11 11:02:36 +0200

    fix: use explicit typing with Tuple and Union in to_kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Convert a key and numeric value into a tuple."""
 
+from typing import Union, Tuple
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with the key and value squared."""
     return (k, float(v * v))
