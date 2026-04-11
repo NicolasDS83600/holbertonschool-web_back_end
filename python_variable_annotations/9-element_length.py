a3b3b13177970dad9fbd3a45f24fd4a7d378f339
NicolasDS83600 <nico836001@outlook.fr>
2026-04-11 11:07:35 +0200

    feat: use duck-typed Iterable and Sequence for element_length annotations

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Provide lengths for each string in a list."""
-from typing import List, Tuple
+
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return pairs of string and its length."""
     return [(i, len(i)) for i in lst]
