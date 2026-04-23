517da09699af0c4e5e342edde483ca2395b261ff
NicolasDS83600 <nico836001@outlook.fr>
2026-04-23 13:58:06 +0200

    fix: add __name == __main__

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,17 +3,18 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-collection = client.logs.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-total = collection.count_documents({})
-print(f"{total} logs")
+    total = collection.count_documents({})
+    print(f"{total} logs")
 
-print("Methods:")
-for m in methods:
-    count = collection.count_documents({"method": m})
-    print(f"\tmethod {m}: {count}")
+    print("Methods:")
+    for m in methods:
+        count = collection.count_documents({"method": m})
+        print(f"\tmethod {m}: {count}")
 
-status = collection.count_documents({"method": "GET", "path": "/status"})
-print(f"{status} status check")
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
