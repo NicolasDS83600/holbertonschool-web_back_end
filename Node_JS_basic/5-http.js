3d4b74fe336afc2c746df294a19dab1f33d11213
NicolasDS83600 <nico836001@outlook.fr>
2026-05-13 09:42:12 +0200

    feat(nodejs): add JavaScript scripts for Node.js I/O and HTTP operations
    - Add 0-console.js: basic console output script
    - Add 1-stdin.js: handle standard input from user
    - Add 2-read_file.js: synchronous file reading using fs module
    - Add 3-read_file_async.js: asynchronous file reading using fs module
    - Add 4-http.js: basic HTTP server implementation
    - Add 5-http.js: extended HTTP server implementation
    - Add README.md: project documentation and usage instructions

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const countStudents = require("./3-read_file_async");
+
+const app = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+
+    const url = req.url;
+
+    if (url === "/") {
+        res.statusCode = 200;
+        res.end("Hello Holberton School!");
+    } else if (url === "/students") {
+        const database = process.argv[2];
+
+        res.statusCode = 200;
+
+        countStudents(database)
+            .then(() => {
+                res.end("This is the list of our students");
+            })
+            .catch(() => {
+                res.end("This is the list of our students\nCannot load the database");
+            });
+    } else {
+        res.statusCode = 404;
+        res.end("Not Found");
+    }
+});
+
+app.listen(1245);
+
+module.exports = app;
