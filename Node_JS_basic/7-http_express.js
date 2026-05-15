e96afef4966d7d8fbaa8798719e49c2cc82e8393
NicolasDS83600 <nico836001@outlook.fr>
2026-05-15 11:34:10 +0200

    feat: add express.js scipt

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, "utf8", (err, data) => {
+            if (err) {
+                reject(new Error("Cannot load the database"));
+                return;
+            }
+
+            const lines = data
+                .split("\n")
+                .filter((line) => line.trim() !== "");
+
+            const students = lines.slice(1);
+
+            const fields = {};
+
+            students.forEach((student) => {
+                const parts = student.split(",");
+                const firstname = parts[0];
+                const field = parts[3];
+
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+
+                fields[field].push(firstname);
+            });
+
+            let output = `Number of students: ${students.length}`;
+
+            Object.keys(fields).forEach((field) => {
+                output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(", ")}`;
+            });
+
+            resolve(output);
+        });
+    });
+}
+
+app.get("/", (req, res) => {
+    res.type("text/plain");
+    res.send("Hello Holberton School!");
+});
+
+app.get("/students", async (req, res) => {
+    res.type("text/plain");
+
+    const dbPath = process.argv[2];
+
+    try {
+        const students = await countStudents(dbPath);
+        res.send(`This is the list of our students\n${students}`);
+    } catch (err) {
+        res.send(`This is the list of our students\n${err.message}`);
+    }
+});
+
+app.listen(1245);
+
+module.exports = app;
---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,45 @@
+import readDatabase from "../utils";
+
+class StudentsController {
+    static async getAllStudents(req, res) {
+        const dbPath = process.argv[2];
+
+        try {
+            const students = await readDatabase(dbPath);
+
+            let response = "This is the list of our students";
+
+            const fields = Object.keys(students).sort((a, b) => (
+                a.localeCompare(b, undefined, { sensitivity: "base" })
+            ));
+
+            fields.forEach((field) => {
+                response += `\nNumber of students in ${field}: ${students[field].length}. List: ${students[field].join(", ")}`;
+            });
+
+            res.status(200).send(response);
+        } catch (err) {
+            res.status(500).send("Cannot load the database");
+        }
+    }
+
+    static async getAllStudentsByMajor(req, res) {
+        const dbPath = process.argv[2];
+        const { major } = req.params;
+
+        if (major !== "CS" && major !== "SWE") {
+            res.status(500).send("Major parameter must be CS or SWE");
+            return;
+        }
+
+        try {
+            const students = await readDatabase(dbPath);
+
+            res.status(200).send(`List: ${students[major].join(", ")}`);
+        } catch (err) {
+            res.status(500).send("Cannot load the database");
+        }
+    }
+}
+
+export default StudentsController;
